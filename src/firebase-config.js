b244424d73e670cd16a4da25027477dd96bb44f6
adelelohzh <adeleloh@comp.nus.edu.sg>
2021-10-17 12:22:25 +0800

    added env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import {initializeApp} from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBDDcRjD-n8GARKy7Kz04UMPyJ_2DMW3lY",
-    authDomain: "customhrms.firebaseapp.com",
-    projectId: "customhrms",
-    storageBucket: "customhrms.appspot.com",
-    messagingSenderId: "454637246867",
-    appId: "1:454637246867:web:7d3d4402869f5a20ac3066",
-    measurementId: "G-KV472W0W56"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MS_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
