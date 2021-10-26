a60e4e43cc50512c7f5156efe4f4c101dc5dd9b6
zululim <e0425951@u.nus.edu>
2021-10-26 22:04:34 +0800

    added more log statements

--- src/__mocks__/LeaveApplications.js
+++ src/__mocks__/LeaveApplications.js
@@ -24,5 +24,6 @@ const leaveApplicationsList = async function getLeaveApplications() {
 
 const leaveApplications = Array.from(leaveApplicationsList);
 console.log('leaveApplications type:', typeof (leaveApplications));
+console.log('leaveApplications:', leaveApplications);
 
 export default { leaveApplications };
