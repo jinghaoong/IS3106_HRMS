81c874f544e3203f3876841192f7f2152bbc0bd3
Jing Hao Ong <jinghaoong@gmail.com>
2021-11-09 00:31:35 +0800

    added payslip components

---
+++ src/components/payroll/PayslipPdf.js
@@ -0,0 +1,152 @@
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle
+} from '@material-ui/core';
+import {
+  Document,
+  Page,
+  PDFViewer,
+  StyleSheet,
+  Text,
+  View
+} from '@react-pdf/renderer';
+import { format } from 'date-fns';
+import { PropTypes } from 'prop-types';
+
+const PayslipPdf = ({ payslip, open, handleClose }) => {
+  const styles = StyleSheet.create({
+    page: {
+      fontSize: 16,
+      flexDirection: 'column',
+      marginTop: 16,
+      marginBottom: 16,
+      marginLeft: 16,
+      marginRight: 16,
+    },
+    titleContainer: {
+      fontSize: 24,
+      alignSelf: 'left',
+      marginBottom: 15,
+    },
+    title: {
+      fontWeight: 'bold',
+    },
+    headerContainer: {
+      alignSelf: 'left',
+      flexDirection: 'row',
+      width: '70%',
+      marginBottom: 5,
+    },
+    headerTitle: {
+      width: '40%',
+    },
+    headerInput: {
+      width: '60%'
+    },
+    tableContainer: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      width: '80%',
+      alignSelf: 'center',
+      marginTop: 10,
+    },
+    row: {
+      border: '1',
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    description: {
+      borderRight: '1',
+      width: '50%',
+    },
+    xyz: {
+      width: '50%',
+    }
+  });
+
+  const PayslipDocument = () => (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>COMPANY PAYSLIP</Text>
+        </View>
+
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerTitle}>Employee Email</Text>
+          <Text style={styles.headerInput}>{payslip.email}</Text>
+        </View>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerTitle}>Payment Mode</Text>
+          <Text style={styles.headerInput}>{payslip.paymentMode}</Text>
+        </View>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerTitle}>Payment Date</Text>
+          <Text style={styles.headerInput}>
+            {format((payslip.startDate.toDate()), 'dd-MM-yyyy')}
+            &nbsp;to&nbsp;
+            {format((payslip.endDate.toDate()), 'dd-MM-yyyy')}
+          </Text>
+        </View>
+
+        <View style={styles.tableContainer}>
+          <View style={styles.row}>
+            <Text style={styles.description}>Basic</Text>
+            <Text style={styles.xyz}>{payslip.basic}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.description}>CPF</Text>
+            <Text style={styles.xyz}>{payslip.cpfEmployee + payslip.cpfEmployer}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.description}>Overtime</Text>
+            <Text style={styles.xyz}>{payslip.overtime > 0 ? payslip.overtime : 0}</Text>
+          </View>
+        </View>
+
+      </Page>
+    </Document>
+  );
+
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="800">
+      <DialogTitle>
+        <div>
+          Print Payslip: &ldquo;
+          {payslip.id}
+          &ldquo;
+        </div>
+      </DialogTitle>
+      <DialogContent>
+        <Box
+          sx={{
+            backgroundColor: 'background.default',
+            display: 'flex',
+            flexDirection: 'column',
+            with: '100%',
+            height: '100%',
+            justifyContent: 'center'
+          }}
+        >
+          <PDFViewer width="500" height="800">
+            <PayslipDocument />
+          </PDFViewer>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+PayslipPdf.propTypes = {
+  payslip: PropTypes.object,
+  open: PropTypes.bool,
+  handleClose: PropTypes.func,
+};
+
+export default PayslipPdf;
--- src/pages/Payroll.js
+++ src/pages/Payroll.js
@@ -1,43 +1,27 @@
-import { Helmet } from 'react-helmet';
-// import { PropTypes } from 'prop-types';
+// import { PropTypes } from 'prop-types'
 import {
-  // Alert,
   Box,
   Button,
   ButtonGroup,
   Container,
-  IconButton,
-  Dialog,
-  DialogActions,
-  DialogTitle,
-  DialogContent,
-  TextField,
-  // Typography,
+  IconButton
 } from '@material-ui/core';
-import { InputAdornment } from '@mui/material';
 import {
-  Add as AddIcon,
-  Edit as EditIcon,
-  Delete as DeleteIcon
+  Add as AddIcon, Delete as DeleteIcon, Edit as EditIcon, PictureAsPdf as PictureAsPdfIcon
 } from '@mui/icons-material';
 import {
-  collection,
-  getDocs,
-  doc,
-  setDoc,
-  deleteDoc,
-  Timestamp,
-} from 'firebase/firestore';
-import { useState, useEffect, } from 'react';
-import {
   DataGrid,
-  GridToolbar,
+  GridToolbar
 } from '@mui/x-data-grid';
-import DateTimePicker from '@mui/lab/DateTimePicker';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { format } from 'date-fns';
-
+import {
+  collection,
+  getDocs
+} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import PayslipDelete from '../components/payroll/PayslipDelete';
+import PayslipForm from '../components/payroll/PayslipForm';
+import PayslipPdf from '../components/payroll/PayslipPdf';
 import { db } from '../firebase-config';
 
 const Payroll = () => {
@@ -103,6 +87,15 @@ const Payroll = () => {
       }}
       >
         <DeleteIcon />
+      </IconButton>
+      <IconButton onClick={() => {
+        setDialogType('pdf');
+        setPayslip(params.row);
+        console.log(dialogType);
+        handleClickOpen();
+      }}
+      >
+        <PictureAsPdfIcon />
       </IconButton>
     </ButtonGroup>
   );
@@ -187,271 +180,30 @@ const Payroll = () => {
                 }}
               />
             </div>
-            <Dialog open={open} onClose={handleClose}>
-              <DialogTitle>
-                {dialogType === 'create' && <div>Create Payslip</div>}
-                {dialogType === 'edit' && <div>Edit Payslip</div>}
-                {dialogType === 'delete' && <div>Delete Payslip</div>}
-              </DialogTitle>
-              {dialogType !== 'delete'
-                && (
-                  <DialogContent>
-                    <Box
-                      sx={{
-                        backgroundColor: 'background.default',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        height: '100%',
-                        justifyContent: 'center'
-                      }}
-                    >
-                      <Container maxWidth="sm">
-                        <Formik
-                          initialValues={{
-                            id: payslip.id,
-                            email: payslip.email,
-                            basic: payslip.basic,
-                            overtime: payslip.overtime,
-                            cpfEmployee: payslip.cpfEmployee,
-                            cpfEmployer: payslip.cpfEmployer,
-                            startDate: payslip.startDate === undefined
-                              ? new Date() : payslip.startDate.toDate(),
-                            endDate: payslip.endDate === undefined
-                              ? new Date() : payslip.endDate.toDate(),
-                            paymentMode: payslip.paymentMode,
-                            remarks: payslip.remarks === undefined
-                              ? '' : payslip.remarks,
-                          }}
-                          validationSchema={Yup.object().shape({
-                            email: Yup.string().email('Must be a valid email').max(255).required('Enter Employee Email'),
-                            basic: Yup.number().required('Enter Basic Pay'),
-                            overtime: Yup.number().required('Enter Overtime Pay'),
-                            cpfEmployee: Yup.number(),
-                            cpfEmployer: Yup.number(),
-                            startDate: Yup.date().required('Enter Start Date'),
-                            endDate: Yup.date().required('Enter End Date'),
-                            paymentMode: Yup.string().required('Enter Payment Mode'),
-                            remarks: Yup.string(),
-                          })}
-                          onSubmit={(values, actions) => {
-                            const newId = `${values.email}_${format(values.endDate, 'dd-MM-yyyy')}`;
-
-                            const temp = {
-                              id: newId,
-                              email: values.email,
-                              basic: values.basic,
-                              overtime: values.overtime,
-                              cpfEmployee: values.cpfEmployee,
-                              cpfEmployer: values.cpfEmployer,
-                              startDate: Timestamp.fromDate(values.startDate),
-                              endDate: Timestamp.fromDate(values.endDate),
-                              paymentMode: values.paymentMode,
-                              remarks: values.remarks
-                            };
-
-                            setPayslip(temp);
-
-                            if (dialogType === 'edit') {
-                              deleteDoc(doc(db, 'payroll', values.id));
-                            }
-
-                            setDoc(doc(db, 'payroll', newId), temp)
-                              .then(() => {
-                                console.log('doc set');
-                                handleClose();
-                              })
-                              .catch((error) => {
-                                console.log(error.message);
-                                actions.setErrors({ submit: error.message });
-                                actions.setSubmitting(false);
-                              });
-                          }}
-                        >
-                          {({
-                            errors,
-                            handleBlur,
-                            handleChange,
-                            handleSubmit,
-                            setFieldValue,
-                            isSubmitting,
-                            touched,
-                            values
-                          }) => (
-                            <form onSubmit={handleSubmit}>
-                              <TextField
-                                error={Boolean(touched.email && errors.email)}
-                                fullWidth
-                                helperText={touched.email && errors.email}
-                                label="Employee Email"
-                                margin="normal"
-                                name="email"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                type="string"
-                                value={values.email}
-                                variant="outlined"
-                                InputProps={{
-                                  readOnly: dialogType === 'edit'
-                                }}
-                              />
-                              <TextField
-                                error={Boolean(touched.basic && errors.basic)}
-                                fullWidth
-                                helperText={touched.basic && errors.basic}
-                                label="Basic Pay"
-                                margin="normal"
-                                name="basic"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                type="number"
-                                value={values.basic}
-                                variant="outlined"
-                                InputProps={{
-                                  startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                                }}
-                              />
-                              <TextField
-                                error={Boolean(touched.overtime && errors.overtime)}
-                                fullWidth
-                                helperText={touched.overtime && errors.overtime}
-                                label="Overtime Pay"
-                                margin="normal"
-                                name="overtime"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                type="number"
-                                value={values.overtime}
-                                variant="outlined"
-                                InputProps={{
-                                  startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                                }}
-                              />
-                              <TextField
-                                error={Boolean(touched.cpfEmployee && errors.cpfEmployee)}
-                                fullWidth
-                                helperText={touched.cpfEmployee && errors.cpfEmployee}
-                                label="Employee CPF"
-                                margin="normal"
-                                name="cpfEmployee"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                type="number"
-                                value={values.cpfEmployee}
-                                variant="outlined"
-                                InputProps={{
-                                  startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                                }}
-                              />
-                              <TextField
-                                error={Boolean(touched.cpfEmployer && errors.cpfEmployer)}
-                                fullWidth
-                                helperText={touched.cpfEmployer && errors.cpfEmployer}
-                                label="Employer CPF"
-                                margin="normal"
-                                name="cpfEmployer"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                type="number"
-                                value={values.cpfEmployer}
-                                variant="outlined"
-                                InputProps={{
-                                  startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                                }}
-                              />
-                              <DateTimePicker
-                                renderInput={(params) => <TextField {...params} />}
-                                label="Start Date"
-                                name="startDate"
-                                value={values.startDate}
-                                onChange={(newStart) => {
-                                  setFieldValue('startDate', newStart);
-                                }}
-                              />
-                              <DateTimePicker
-                                renderInput={(params) => <TextField {...params} />}
-                                label="End Date"
-                                name="endDate"
-                                value={values.endDate}
-                                onChange={(newEnd) => {
-                                  setFieldValue('endDate', newEnd);
-                                }}
-                              />
-                              <TextField
-                                error={Boolean(touched.paymentMode && errors.paymentMode)}
-                                fullWidth
-                                helperText={touched.paymentMode && errors.paymentMode}
-                                label="Payment Mode"
-                                margin="normal"
-                                name="paymentMode"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                type="string"
-                                value={values.paymentMode}
-                                variant="outlined"
-                              />
-                              <TextField
-                                error={Boolean(touched.remarks && errors.remarks)}
-                                fullWidth
-                                helperText={touched.remarks && errors.remarks}
-                                label="Remarks"
-                                margin="normal"
-                                name="remarks"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                type="string"
-                                value={values.remarks}
-                                variant="outlined"
-                              />
-                              <Box sx={{ py: 2 }}>
-                                <Button
-                                  color="primary"
-                                  disabled={isSubmitting}
-                                  fullWidth
-                                  size="large"
-                                  type="submit"
-                                  variant="contained"
-                                >
-                                  Submit
-                                </Button>
-                              </Box>
-                            </form>
-                          )}
-                        </Formik>
-                      </Container>
-                    </Box>
-                  </DialogContent>
-                )}
-              {dialogType === 'delete'
-                && (
-                  <DialogContent>
-                    Confirm Deletion of Payslip ID:&nbsp;
-                    <b>{payslip.id}</b>
-                  </DialogContent>
-                )}
-              <DialogActions>
-                {dialogType === 'delete'
-                  && (
-                    <Button
-                      color="error"
-                      fullWidth
-                      variant="contained"
-                      onClick={() => {
-                        deleteDoc(doc(db, 'payroll', payslip.id))
-                          .then(() => {
-                            console.log('Payslip deleted');
-                            handleClose();
-                          })
-                          .catch((error) => {
-                            console.log(error.message);
-                          });
-                      }}
-                    >
-                      Confirm
-                    </Button>
-                  )}
-                <Button onClick={handleClose}>Cancel</Button>
-              </DialogActions>
-            </Dialog>
+            {(dialogType === 'create' || dialogType === 'edit')
+              && (
+                <PayslipForm
+                  payslip={payslip}
+                  setPayslip={setPayslip}
+                  dialogType={dialogType}
+                  open={open}
+                  handleClose={handleClose}
+                />
+              )}
+            {dialogType === 'delete' && (
+              <PayslipDelete
+                payslip={payslip}
+                open={open}
+                handleClose={handleClose}
+              />
+            )}
+            {dialogType === 'pdf' && (
+              <PayslipPdf
+                payslip={payslip}
+                open={open}
+                handleClose={handleClose}
+              />
+            )}
           </Box>
         </Container>
       </Box>
