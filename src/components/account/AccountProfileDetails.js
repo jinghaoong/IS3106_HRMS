16524b352697e94a4782be7c82acc01908994e15
valenciateh-nus <81210026+valenciateh-nus@users.noreply.github.com>
2021-11-10 04:10:29 +0800

    edited AccountProfileDetails

--- src/components/account/AccountProfileDetails.js
+++ src/components/account/AccountProfileDetails.js
@@ -1,5 +1,8 @@
+import moment from 'moment';
 import {
+  Avatar,
   Box,
+  Typography,
   Button,
   Card,
   CardContent,
@@ -86,6 +89,9 @@ const AccountProfileDetails = () => {
     getEmp();
   }, []);
 
+  const empDOB = currEmp[0].dob ? moment(currEmp[0].dob.toDate()).calendar() : '';
+  const empStart = currEmp[0].startDate ? moment(currEmp[0].startDate.toDate()).calendar() : '';
+
   console.log(currEmp);
   console.log(currEmp);
 
@@ -103,146 +109,212 @@ const AccountProfileDetails = () => {
   };
 
   return (
-    <form
-      autoComplete="off"
-      noValidate
-      handleSubmit
-      {...values[0]}
-    >
-      <Card>
-        <CardHeader
-          subheader="Edit your profile details here."
-          title="Profile"
-        />
-        <Divider />
-        <CardContent>
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                helperText="Please specify the first name"
-                label="First name"
-                name="firstName"
-                onChange={handleChange}
-                required
-                value={values.firstName}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Last name"
-                name="lastName"
-                onChange={handleChange}
-                required
-                value={values.lastName}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <DateTimePicker
-                renderInput={(params) => <TextField {...params} />}
-                label="Date of Birth"
-                name="dob"
-                value={values.dob}
-                required
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Phone Number"
-                name="phone"
-                onChange={handleChange}
-                type="number"
-                value={values.contact}
-                variant="outlined"
-                required
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="IC Number"
-                name="identificationNo"
-                onChange={handleChange}
-                required
-                value={values.identificationNo}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Select Bank"
-                name="bank"
-                onChange={handleChange}
-                required
-                select
-                SelectProps={{ native: true }}
-                value={values.bank}
-                variant="outlined"
-              >
-                {banks.map((option) => (
-                  <option
-                    key={option.value}
-                    value={option.value}
-                  >
-                    {option.label}
-                  </option>
-                ))}
-              </TextField>
-            </Grid>
-          </Grid>
-        </CardContent>
-        <Divider />
-        <Box
+    <>
+      <Box
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <Avatar
+          src={currEmp[0].avatar}
           sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            p: 2
+            height: 100,
+            width: 100
           }}
+        />
+        <Typography
+          color="textPrimary"
+          gutterBottom
+          variant="h3"
         >
-          <Button
-            color="primary"
-            variant="contained"
-            type="submit"
-            value="submit"
-            onClick={handleSubmit}
+          {`${currEmp[0].firstName} ${currEmp[0].lastName}`}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="body1"
+        >
+          {currEmp[0].identificationNo}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="body1"
+        >
+          {currEmp[0].email}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="body1"
+        >
+          {currEmp[0].address}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="body1"
+        >
+          {currEmp[0].role}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="body1"
+        >
+          {currEmp[0].status}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="body1"
+        >
+          {empDOB}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="body1"
+        >
+          {empStart}
+        </Typography>
+      </Box>
+      <form
+        autoComplete="off"
+        noValidate
+        handleSubmit
+        {...values[0]}
+      >
+        <Card>
+          <CardHeader
+            subheader="Edit your currEmp[0] details here."
+            title="Profile"
+          />
+          <Divider />
+          <CardContent>
+            <Grid
+              container
+              spacing={3}
+            >
+              <Grid
+                item
+                md={6}
+                xs={12}
+              >
+                <TextField
+                  fullWidth
+                  helperText="Please specify the first name"
+                  label="First name"
+                  name="firstName"
+                  onChange={handleChange}
+                  required
+                  value={values.firstName}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid
+                item
+                md={6}
+                xs={12}
+              >
+                <TextField
+                  fullWidth
+                  label="Last name"
+                  name="lastName"
+                  onChange={handleChange}
+                  required
+                  value={values.lastName}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid
+                item
+                md={6}
+                xs={12}
+              >
+                <DateTimePicker
+                  renderInput={(params) => <TextField {...params} />}
+                  label="Date of Birth"
+                  name="dob"
+                  value={values.dob}
+                  required
+                />
+              </Grid>
+              <Grid
+                item
+                md={6}
+                xs={12}
+              >
+                <TextField
+                  fullWidth
+                  label="Phone Number"
+                  name="phone"
+                  onChange={handleChange}
+                  type="number"
+                  value={values.contact}
+                  variant="outlined"
+                  required
+                />
+              </Grid>
+              <Grid
+                item
+                md={6}
+                xs={12}
+              >
+                <TextField
+                  fullWidth
+                  label="IC Number"
+                  name="identificationNo"
+                  onChange={handleChange}
+                  required
+                  value={values.identificationNo}
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid
+                item
+                md={6}
+                xs={12}
+              >
+                <TextField
+                  fullWidth
+                  label="Select Bank"
+                  name="bank"
+                  onChange={handleChange}
+                  required
+                  select
+                  SelectProps={{ native: true }}
+                  value={values.bank}
+                  variant="outlined"
+                >
+                  {banks.map((option) => (
+                    <option
+                      key={option.value}
+                      value={option.value}
+                    >
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </Grid>
+            </Grid>
+          </CardContent>
+          <Divider />
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              p: 2
+            }}
           >
-            Save details
-          </Button>
-        </Box>
-      </Card>
-    </form>
+            <Button
+              color="primary"
+              variant="contained"
+              type="submit"
+              value="submit"
+              onClick={handleSubmit}
+            >
+              Save details
+            </Button>
+          </Box>
+        </Card>
+      </form>
+    </>
   );
 };
 
