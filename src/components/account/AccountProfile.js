6f86ba1b409b1e645cb6781a5272c1ec97f6a855
valenciateh-nus <81210026+valenciateh-nus@users.noreply.github.com>
2021-11-10 04:01:55 +0800

    edited AccountProfile

--- src/components/account/AccountProfile.js
+++ src/components/account/AccountProfile.js
@@ -5,7 +5,6 @@ import {
   Typography
 } from '@material-ui/core';
 import { useEffect, useState } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
 import {
   doc,
   getDoc
@@ -14,61 +13,44 @@ from 'firebase/firestore';
 import { auth, db } from '../../firebase-config';
 
 const AccountProfile = () => {
-  const [currUser, setCurrUser] = useState([]);
-  const [currEmp, setCurrEmp] = useState([]);
+  const [currEmp, setCurrEmp] = useState('');
 
-  const profileVal = {
-    avatar: '/static/images/avatars/default_avatar.png',
-    firstName: '',
-    lastName: '',
-    identificationNo: '',
-    email: '',
-    contact: '',
-    address: '',
-    role: '',
-    status: '',
-    dob: '',
-    joined: ''
-  };
-
-  const [profile, setProfile] = useState(profileVal);
-
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setCurrUser(user);
-    } else {
-      setCurrUser(null);
-    }
-  });
-
-  const getEmp = async () => {
-    await getDoc(doc(db, 'users', `${currUser.email}`)).then((docSnap) => {
+  const currEmployee = async () => {
+    await getDoc(doc(db, 'users', (auth.currentUser).email)).then((docSnap) => {
       if (docSnap.exists()) {
-        const newData = docSnap.data();
-        setCurrEmp(newData);
-        setProfile({
-          avatar: '/static/images/avatars/default_avatar.png',
-          firstName: currEmp.firstName,
-          lastName: currEmp.lastName,
-          identificationNo: currEmp.identificationNo,
-          email: currEmp.email,
-          contact: currEmp.contact,
-          address: currEmp.address,
-          role: currEmp.role,
-          status: currEmp.status,
-          dob: currEmp.dob,
-          startDate: currEmp.startDate
-        });
+        console.log('Document data:', docSnap.data());
+        setCurrEmp(docSnap.data());
+        console.log('Current employee is ', currEmp);
+      } else {
+        console.log('No such document!');
       }
     });
   };
 
   useEffect(() => {
-    getEmp();
-  }, [currUser, currEmp]);
+    currEmployee();
+  }, []);
+
+  console.log('Testing', currEmp.firstName);
+
+  const profile = {
+    avatar: '/static/images/avatars/default_avatar.png',
+    firstName: currEmp.firstName,
+    lastName: currEmp.lastName,
+    identificationNo: currEmp.identificationNo,
+    email: currEmp.email,
+    contact: currEmp.contact,
+    address: currEmp.address,
+    role: currEmp.role,
+    status: currEmp.status,
+    dob: currEmp.dob,
+    joined: currEmp.startDate
+  };
 
   const empDOB = profile.dob ? moment(profile.dob.toDate()).calendar() : '';
   const empStart = profile.startDate ? moment(profile.startDate.toDate()).calendar() : '';
+
+  console.log(currEmp.firstName);
 
   return (
     <Box
@@ -87,16 +69,10 @@ const AccountProfile = () => {
       />
       <Typography
         color="textPrimary"
+        gutterBottom
         variant="h3"
       >
         {`${profile.firstName} ${profile.lastName}`}
-      </Typography>
-      <Typography
-        color="textPrimary"
-        gutterBottom
-        variant="h5"
-      >
-        {`${profile.role}`}
       </Typography>
       <Typography
         color="textSecondary"
@@ -120,20 +96,24 @@ const AccountProfile = () => {
         color="textSecondary"
         variant="body1"
       >
+        {profile.role}
+      </Typography>
+      <Typography
+        color="textSecondary"
+        variant="body1"
+      >
         {profile.status}
       </Typography>
       <Typography
         color="textSecondary"
         variant="body1"
       >
-        Date of Birth:&nbsp;
         {empDOB}
       </Typography>
       <Typography
         color="textSecondary"
         variant="body1"
       >
-        Joined On:&nbsp;
         {empStart}
       </Typography>
     </Box>
