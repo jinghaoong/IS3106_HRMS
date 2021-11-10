8eb0337a94d5662efa4a522b7ab070ceaa887fb9
zululim <e0425951@u.nus.edu>
2021-11-10 21:20:34 +0800

    CRLF fixes

--- src/components/account/AccountProfileDetails.js
+++ src/components/account/AccountProfileDetails.js
@@ -9,20 +9,14 @@ import {
   Grid,
   TextField
 } from '@material-ui/core';
-<<<<<<< HEAD
-import { updateDoc, doc, getDoc } from 'firebase/firestore';
-import { onAuthStateChanged } from 'firebase/auth';
-// import { db } from '../firebase-config';
-=======
 import {
   updateDoc,
   getDoc,
   doc,
 } from 'firebase/firestore';
->>>>>>> dd5a72bebf6809d02ae6aec54d16b992ed7f58b1
 import { useEffect, useState } from 'react';
-import DateTimePicker from '@mui/lab/DateTimePicker';
-import AccountProfile from './AccountProfile';
+import { DateTimePicker } from '@mui/lab/DateTimePicker';
+import { AccountProfile } from './AccountProfile';
 import { db, auth } from '../../firebase-config';
 
 const banks = [
@@ -106,90 +100,6 @@ const AccountProfileDetails = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <form autoComplete="off" noValidate handleSubmit {...props}>
-      <Card>
-        <CardHeader
-          subheader="Edit your profile details here."
-          title="Profile"
-        />
-        <Divider />
-        <CardContent>
-          <Grid container spacing={3}>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                helperText="Please specify the first name"
-                label="First name"
-                name="firstName"
-                onChange={handleChange}
-                required
-                value={values.firstName}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="Last name"
-                name="lastName"
-                onChange={handleChange}
-                required
-                value={values.lastName}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <DateTimePicker
-                renderInput={(params) => <TextField {...params} />}
-                label="Date of Birth"
-                name="dob"
-                value={values.dob}
-                required
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="Phone Number"
-                name="phone"
-                onChange={handleChange}
-                type="number"
-                value={values.contact}
-                variant="outlined"
-                required
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="IC Number"
-                name="identificationNo"
-                onChange={handleChange}
-                required
-                value={values.identificationNo}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="Select Bank"
-                name="bank"
-                onChange={handleChange}
-                required
-                select
-                SelectProps={{ native: true }}
-                value={values.bank}
-                variant="outlined"
-              >
-                {banks.map((option) => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </TextField>
-=======
     <>
       <AccountProfile />
       <form
@@ -310,7 +220,6 @@ const AccountProfileDetails = () => {
                   ))}
                 </TextField>
               </Grid>
->>>>>>> dd5a72bebf6809d02ae6aec54d16b992ed7f58b1
             </Grid>
           </CardContent>
           <Divider />
