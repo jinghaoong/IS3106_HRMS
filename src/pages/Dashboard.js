a6201edd060fda60bccfdc807f3922b98ef0bfbc
valenciateh-nus <81210026+valenciateh-nus@users.noreply.github.com>
2021-11-11 17:25:36 +0800

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,3 @@
-import { makeStyles } from '@material-ui/styles';
 import { Helmet } from 'react-helmet';
 import { Box, Container, Grid } from '@material-ui/core';
 import LateAttendances from '../components/dashboard/LateAttendances';
@@ -10,126 +9,99 @@ import TotalEmployees from '../components/dashboard/TotalEmployees';
 import UnapprovedLeaves from '../components/dashboard/UnapprovedLeaves';
 import TrafficByDevice from '../components/dashboard/TrafficByDevice';
 
-const useStyles = makeStyles((theme) => ({
-  hero: {
-    backgroundColor: 'white',
-    height: '200px',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    position: 'relative',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'black',
-    fontSize: '4rem',
-    [theme.breakpoints.down('sm')]: {
-      height: 300,
-      fontSize: '3em'
-    },
-    fontFamily: 'sans-serif'
-  }
-}));
-
-function Dashboard() {
-  const classes = useStyles();
-  return (
-    <>
-      <Helmet>
-        <title>Dashboard | Material Kit</title>
-      </Helmet>
-      <Box className={classes.hero}>
-        <Box>Welcome to the HRMS Portal</Box>
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: 'background.default',
-          minHeight: '100%',
-          py: 3
-        }}
-      >
-        <Container maxWidth={false}>
+const Dashboard = () => (
+  <>
+    <Helmet>
+      <title>Dashboard | Material Kit</title>
+    </Helmet>
+    <Box
+      sx={{
+        backgroundColor: 'background.default',
+        minHeight: '100%',
+        py: 3
+      }}
+    >
+      <Container maxWidth={false}>
+        <Grid
+          container
+          spacing={3}
+        >
           <Grid
-            container
-            spacing={3}
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
           >
-            <Grid
-              item
-              lg={3}
-              sm={6}
-              xl={3}
-              xs={12}
-            >
-              <LateAttendances />
-            </Grid>
-            <Grid
-              item
-              lg={3}
-              sm={6}
-              xl={3}
-              xs={12}
-            >
-              <TotalEmployees />
-            </Grid>
-            <Grid
-              item
-              lg={3}
-              sm={6}
-              xl={3}
-              xs={12}
-            >
-              <TasksProgress />
-            </Grid>
-            <Grid
-              item
-              lg={3}
-              sm={6}
-              xl={3}
-              xs={12}
-            >
-              <UnapprovedLeaves sx={{ height: '100%' }} />
-            </Grid>
-            <Grid
-              item
-              lg={8}
-              md={12}
-              xl={9}
-              xs={12}
-            >
-              <Sales />
-            </Grid>
-            <Grid
-              item
-              lg={4}
-              md={6}
-              xl={3}
-              xs={12}
-            >
-              <TrafficByDevice sx={{ height: '100%' }} />
-            </Grid>
-            <Grid
-              item
-              lg={4}
-              md={6}
-              xl={3}
-              xs={12}
-            >
-              <LatestProducts sx={{ height: '100%' }} />
-            </Grid>
-            <Grid
-              item
-              lg={8}
-              md={12}
-              xl={9}
-              xs={12}
-            >
-              <LatestOrders />
-            </Grid>
+            <LateAttendances />
           </Grid>
-        </Container>
-      </Box>
-    </>
-  );
-}
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <TotalEmployees />
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <TasksProgress />
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            sm={6}
+            xl={3}
+            xs={12}
+          >
+            <UnapprovedLeaves sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <Sales />
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            <TrafficByDevice sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xl={3}
+            xs={12}
+          >
+            <LatestProducts sx={{ height: '100%' }} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <LatestOrders />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+);
 
 export default Dashboard;
